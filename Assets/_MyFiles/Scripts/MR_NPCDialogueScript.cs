a0398ccf53cf09efac42d71d3918c2ec32ebfb9e
Manuel Rodriguez <mgrodri5@student.uiwtx.edu>
2023-11-20 10:59:36 -0600

    Game Set

--- Assets/_MyFiles/Scripts/MR_NPCDialogueScript.cs
+++ Assets/_MyFiles/Scripts/MR_NPCDialogueScript.cs
@@ -3,25 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 namespace MR
 {
     public class MR_NPCDialogueScript : MonoBehaviour
     {
-        [SerializeField] int requiredPoints;
         [SerializeField] bool talkedTo;
         [SerializeField] GameObject interactionKey;
+
+        [Header("ReputationPoints")]
+        [SerializeField] float highPoints = 100;
+        [SerializeField] float mediumPoints;
+        [SerializeField] float lowPoints;
 
         [Header("OutsideScripts")]
         [SerializeField] MR_DialogueBehavior dialogueBehavior;
         [SerializeField] MR_ReputationScript reputationPoints;
-        [SerializeField] PlayerInputsScript playerInputsScript;
+        [SerializeField] PlayerInputsScript playerInputs;
+        [SerializeField] MR_PlayerMovementScript player;
 
-        [Header("Dialogue Choices")]
-        [SerializeField] MR_DialogueNodeGraph worstReputation;
+        [Header("Dialogue Choices - New Chat")]
         [SerializeField] MR_DialogueNodeGraph lowReputation;
         [SerializeField] MR_DialogueNodeGraph mediumReputation;
         [SerializeField] MR_DialogueNodeGraph highReputation;
+
+        [Header("Dialogue Choices - Talked To")]
         [SerializeField] MR_DialogueNodeGraph lowTalkedTo;
         [SerializeField] MR_DialogueNodeGraph mediumTalkedTo;
         [SerializeField] MR_DialogueNodeGraph highTalkedTo;
@@ -29,27 +36,83 @@ namespace MR
         private void Awake()
         {
             talkedTo = false;
+
+            playerInputs = new PlayerInputsScript();
+            playerInputs.Player.Enable();
+            playerInputs.Player.Interact.performed += Interact;
         }
 
-        public void OnTriggerEnter2D(Collider2D other)
+        public void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player"))
             {
                 return;
             }
 
-            if(reputationPoints.GetReputationPoints() >= requiredPoints)
-            {
+            interactionKey.SetActive(true);
+        }
 
-            }
-            else if (reputationPoints.GetReputationPoints() <= requiredPoints)
+        public void OnTriggerExit(Collider other)
+        {
+            if (!other.CompareTag("Player"))
             {
-
+                return;
             }
-            else
+
+            interactionKey.SetActive(false);
+        }
+
+        public void Interact(InputAction.CallbackContext context)
+        {
+            if (interactionKey.gameObject == true)
             {
-
+                Debug.Log("Test");
+/*                StartDialogue();
+                interactionKey.SetActive(false);
+                player.SetCutscene(true);*/
             }
+        }
+
+        private void StartDialogue()
+        {
+            if (reputationPoints.GetReputationPoints() > mediumPoints || reputationPoints.GetReputationPoints() <= highPoints)
+            {
+                if (!talkedTo)
+                {
+                    dialogueBehavior.StartDialogue(highReputation);
+                    talkedTo = true;
+                }
+                else
+                {
+                    dialogueBehavior.StartDialogue(highTalkedTo);
+                }
+            }
+            else if (reputationPoints.GetReputationPoints() > lowPoints || reputationPoints.GetReputationPoints() <= mediumPoints)
+            {
+                if (!talkedTo)
+                {
+                    dialogueBehavior.StartDialogue(mediumReputation);
+                    talkedTo = true;
+                }
+                else
+                {
+                    dialogueBehavior.StartDialogue(mediumTalkedTo);
+                }
+            }
+            else if (reputationPoints.GetReputationPoints() <= lowPoints)
+            {
+                if (!talkedTo)
+                {
+                    dialogueBehavior.StartDialogue(lowReputation);
+                    talkedTo = true;
+                }
+                else
+                {
+                    dialogueBehavior.StartDialogue(lowTalkedTo);
+                }
+            }
+
+            player.SetCutscene(false);
         }
 
     }
